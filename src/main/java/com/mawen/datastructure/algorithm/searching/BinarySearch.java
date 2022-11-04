656d032545f6728100290e6b5b2b813bdec2cb2e
mawen12 <1181963012mw@gmail.com>
2022-11-04 14:48:47 +0800

    Create BinarySearch.java

---
+++ src/main/java/com/mawen/datastructure/algorithm/searching/BinarySearch.java
@@ -0,0 +1,20 @@
+package com.mawen.datastructure.algorithm.searching;
+
+/**
+ * 二分查找
+ * @author mawen
+ * @since 2022/10/27
+ */
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void binarySearch(int[] arr, int key, int low, int high) {
+        int index = Integer.MAX_VALUE;
+
+
+    }
+
+}
